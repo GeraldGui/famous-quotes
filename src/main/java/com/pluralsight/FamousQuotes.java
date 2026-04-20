69861195c41c4c53877184ae4e0775b504046994
Gerald G <geraldguignard2@gmail.com>
2026-04-20 10:47:30 -0400

    Made quotes in an if statement.

---
+++ src/main/java/com/pluralsight/FamousQuotes.java
@@ -0,0 +1,28 @@
+package com.pluralsight;
+
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class FamousQuotes {
+    public static void main(String[] args) {
+
+        String[] quotes = new String[] {"Be yourself; everyone else is already taken.", "Fall seven times, stand up eight.", "Life’s a short trip. You’ll find out.", "Try to be a rainbow in someone's cloud.",
+        "What we think, we become.", "If you tell the truth, you don't have to remember anything.", "Happiness depends upon ourselves.", "Simplicity is the ultimate sophistication.", "Never regret anything that made you smile.", "Live as if you were to die tomorrow."};
+
+        Scanner myObj = new Scanner(System.in);
+
+        System.out.println("Type a number in 1-10 to display a quote.");
+        int command = myObj.nextInt();
+        myObj.nextLine();
+
+        try {
+            if (command < 10) {
+                System.out.println(quotes[command - 1]);
+            }
+        } catch (Exception e) {
+            System.out.println("Wrong input sorry!");
+            throw new RuntimeException(e);
+        }
+
+    }
+}
